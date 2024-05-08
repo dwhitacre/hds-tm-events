5bc8b7c78639ac1cc1d3f8f4c7ce3741ef85e84c
Dan Whitacre <whitacre.74@osu.edu>
2024-05-07 21:34:38 -0400

    better lb test

--- test/api/leaderboard.cy.js
+++ test/api/leaderboard.cy.js
@@ -12,8 +12,14 @@ describe('/api/leaderboard/{id}', () => {
       url: '/api/leaderboard/standings',
     }).then(response => {
       expect(response.status).to.eq(200)
-      expect(response.body).to.haveOwnProperty('playercount')
       expect(response.body.playercount).to.be.a('number')
+      expect(response.body).to.haveOwnProperty('tops')
+      expect(response.body.tops).to.be.an('array')
+      expect(response.body.tops).to.have.length.gt(0)
+      expect(response.body.tops[0]).to.haveOwnProperty('player')
+      expect(response.body.tops[0].player.name).to.be.a('string')
+      expect(response.body.tops[0].position).to.be.a('number')
+      expect(response.body.tops[0].score).to.be.a('number')
     })
   })
 })

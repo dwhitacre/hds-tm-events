0c0f707c8d5e4672067e9d82c94785290e4db780
Dan Whitacre <whitacre.74@osu.edu>
2024-05-13 21:49:29 -0400

    move lb logic to domain

--- api/api/leaderboard.go
+++ api/api/leaderboard.go
@@ -4,39 +4,21 @@ import (
 	"encoding/json"
 	"hdstmevents-api/domain"
 	"net/http"
-	"os"
 )
 
 func LeaderboardHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.PathValue("id")
-	
-	file, err := os.Open("leaderboard/" + id)
-	
+
+	var leaderboard domain.Leaderboard
+	leaderboard.LeaderboardId = id
+	err := domain.LeaderboardGet(&leaderboard)
 	if err != nil {
 		logger.Warn("No leaderboard found with id", "id", id, "err", err)
 		w.WriteHeader(http.StatusNoContent)
 		return
 	}
-	defer file.Close()
-
-	var leaderboard domain.Leaderboard
-
-	jsonParser := json.NewDecoder(file)
-	if err = jsonParser.Decode(&leaderboard); err != nil {
-		logger.Warn("Leaderboard didnt decode", "id", id, "err", err)
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
 
 	logger.Info("Leaderboard found", "id", id)
-
-	for i := 0; i < len(leaderboard.Tops); i++ {
-		err = domain.PlayerGet(&leaderboard.Tops[i].Player)
-		if err != nil {
-			logger.Warn("Failed to hydrate player", "player", leaderboard.Tops[i].Player)
-		}
-	}
-
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(leaderboard)
 }
--- api/domain/leaderboard.go
+++ api/domain/leaderboard.go
@@ -1,6 +1,40 @@
 package domain
 
+import (
+	"encoding/json"
+	"errors"
+	"os"
+)
+
 type Leaderboard struct {
-	Tops        []Top `json:"tops"`
-	Playercount int   `json:"playercount"`
+	LeaderboardId string 	`json:"leaderboardId"`
+	Tops        	[]*Top 	`json:"tops"`
+	Playercount 	int   	`json:"playercount"`
+}
+
+func LeaderboardGet(leaderboard *Leaderboard) error {
+	if leaderboard.LeaderboardId == "" {
+		return errors.New("LeaderboardGet: missing leaderboardId, nothing to gets")
+	}
+
+	file, err := os.Open("leaderboard/" + leaderboard.LeaderboardId)
+
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	jsonParser := json.NewDecoder(file)
+	if err = jsonParser.Decode(leaderboard); err != nil {
+		return err
+	}
+
+	for i := 0; i < len(leaderboard.Tops); i++ {
+		err = PlayerGet(leaderboard.Tops[i].Player)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }

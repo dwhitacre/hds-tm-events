ac941a5e7a8837215b8396746914f8c75bbcab95
Dan Whitacre <whitacre.74@osu.edu>
2024-06-12 20:58:58 -0400

    remove unused match get handler

--- api/api/match.go
+++ api/api/match.go
@@ -6,24 +6,6 @@ import (
 	"net/http"
 )
 
-func MatchHandler(w http.ResponseWriter, r *http.Request) {
-	id := r.PathValue("id")
-
-	var match domain.Match
-	match.MatchId = id
-	err := domain.MatchGet(&match)
-
-	if err != nil {
-		logger.Warn("No match found with id", "id", id, "err", err)
-		w.WriteHeader(http.StatusNoContent)
-		return
-	}
-
-	logger.Info("Match found", "id", id)
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(match)
-}
-
 func MatchResultHandler(w http.ResponseWriter, r *http.Request) {
 	matchId := r.PathValue("matchId")
 	
